a4124797ef3200a655a086abf614db56110548ba
Felipe Biel <felipe.biel.souza@gmail.com>
2021-01-02 15:59:04 -0300

    new: Base da página de MemoriasPage

---
+++ src/pages/MemoriesPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Todas as Memórias</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <h2>As memórias devem ser adicionadas</h2>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import {
+  IonPage,
+  IonHeader,
+  IonTitle,
+  IonContent,
+  IonToolbar,
+} from "@ionic/vue";
+export default {
+  components: {
+    IonPage,
+    IonHeader,
+    IonTitle,
+    IonContent,
+    IonToolbar,
+  },
+};
+</script>
