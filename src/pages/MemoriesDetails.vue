e65b77222012f4184bb6a09e48b9c326ac374df2
Felipe Biel <felipe.biel.souza@gmail.com>
2021-01-02 16:17:12 -0300

    new: Pagina de details base

---
+++ src/pages/MemoriesDetails.vue
@@ -0,0 +1,39 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Todas as Memórias</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <ion-list>
+        <ion-item>Felipe</ion-item>
+        <ion-item>Davi</ion-item>
+        <ion-item>José</ion-item>
+      </ion-list>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import {
+  IonPage,
+  IonHeader,
+  IonTitle,
+  IonContent,
+  IonToolbar,
+  IonList,
+  IonItem,
+} from "@ionic/vue";
+export default {
+  components: {
+    IonPage,
+    IonHeader,
+    IonTitle,
+    IonContent,
+    IonToolbar,
+    IonList,
+    IonItem,
+  },
+};
+</script>
