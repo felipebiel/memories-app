d382243f13baabc51e1649c0106f22b44659dca0
Felipe Biel <felipe.biel.souza@gmail.com>
2021-01-02 17:35:11 -0300

    new: Pegando a memory correta

--- src/pages/MemoriesDetails.vue
+++ src/pages/MemoriesDetails.vue
@@ -1,9 +1,22 @@
 <template>
   <base-layout pageTitle="Os detalhes" pageDefaultBackLink="/memories">
     <h2>Estou nos detalhes</h2>
+    {{ loadedMemory }}
+    {{ memoryId }}
   </base-layout>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      memoryId: this.$route.params.id,
+    };
+  },
+  computed: {
+    loadedMemory() {
+      return this.$store.getters.memory(this.memoryId);
+    },
+  },
+};
 </script>
