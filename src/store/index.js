41ce6ec712255738f5cf6274cd3af02b5344ff5e
Felipe Biel <felipe.biel.souza@gmail.com>
2021-01-02 17:17:09 -0300

    new: state e getters de memories

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,32 @@
 import { createStore } from 'vuex';
 
 const store = createStore({
-
+    state(){
+        return {
+            memories: [
+                {
+                  img: "https://picsum.photos/500/300?random=1",
+                  id: 1,
+                  title: "Felipe Biel",
+                },
+                {
+                  img: "https://picsum.photos/500/300?random=2",
+                  id: 2,
+                  title: "Davi Biel",
+                },
+                {
+                  img: "https://picsum.photos/500/300?random=3",
+                  id: 3,
+                  title: "Marlucia de Souza",
+                },
+              ],
+        }
+    },
+    getters: {
+        memories(state){
+            return state.memories
+        }
+    }
 })
 
 export default store;
