a7b55b6981065e81e7e102a6cdb98667ef0a8cd3
Felipe Biel <felipe.biel.souza@gmail.com>
2021-01-06 14:25:28 -0300

    new: Colocando a descrição no objeto de retorno do vuex

--- src/store/index.js
+++ src/store/index.js
@@ -8,16 +8,19 @@ const store = createStore({
                   img: "https://picsum.photos/500/300?random=1",
                   id: 1,
                   title: "Felipe Biel",
+                  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu sapien a dui condimentum tristique. Etiam id felis id nibh efficitur consectetur a in sem. "
                 },
                 {
                   img: "https://picsum.photos/500/300?random=2",
                   id: 2,
                   title: "Davi Biel",
+                  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu sapien a dui condimentum tristique. Etiam id felis id nibh efficitur consectetur a in sem. "
                 },
                 {
                   img: "https://picsum.photos/500/300?random=3",
                   id: 3,
                   title: "Marlucia de Souza",
+                  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu sapien a dui condimentum tristique. Etiam id felis id nibh efficitur consectetur a in sem. "
                 },
               ],
         }
