b06fb853a7592a01d9622ee3f77a5962b7bdf33f
Felipe Biel <felipe.biel.souza@gmail.com>
2021-01-07 11:22:57 -0300

    new: Inputs base do form de adição

--- src/pages/AddMemoryPage.vue
+++ src/pages/AddMemoryPage.vue
@@ -1,9 +1,44 @@
 <template>
   <base-layout pageTitle="Nova memória" pageDefaultBackLink="/memories">
-    ADD NEW
+    <form class="ion-padding">
+      <ion-list>
+        <ion-item>
+          <ion-label position="floating">Título</ion-label>
+          <ion-input type="text" required></ion-input>
+        </ion-item>
+        <ion-item>
+          <ion-label position="floating">Imagem</ion-label>
+          <ion-input type="url" required></ion-input>
+        </ion-item>
+      </ion-list>
+      <ion-item>
+        <ion-label position="floating">Descrição</ion-label>
+        <ion-textarea rows="5"></ion-textarea>
+      </ion-item>
+      <ion-button expand="full" class="ion-margin-top" color="tertiary"
+        >Salvar</ion-button
+      >
+    </form>
   </base-layout>
 </template>
 
 <script>
-export default {};
+import {
+  IonList,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonTextarea,
+  IonButton,
+} from "@ionic/vue";
+export default {
+  components: {
+    IonList,
+    IonItem,
+    IonLabel,
+    IonInput,
+    IonTextarea,
+    IonButton,
+  },
+};
 </script>
