733dc037cbb7c0655bdf0814b1828dd5d161eaa7
Felipe Biel <felipe.biel.souza@gmail.com>
2021-01-07 11:27:48 -0300

    new: Componentizando o form de nova memoria

---
+++ src/components/memories/CreateMemoryForm.vue
@@ -0,0 +1,45 @@
+<template>
+  <form class="ion-padding">
+    <ion-list>
+      <ion-item>
+        <ion-label position="floating">Título</ion-label>
+        <ion-input type="text" required></ion-input>
+      </ion-item>
+      <ion-item>
+        <ion-label position="floating">Imagem</ion-label>
+        <ion-input type="url" required></ion-input>
+      </ion-item>
+    </ion-list>
+    <ion-item>
+      <ion-label position="floating">Descrição</ion-label>
+      <ion-textarea rows="5"></ion-textarea>
+    </ion-item>
+    <ion-button expand="full" class="ion-margin-top" color="tertiary"
+      >Salvar</ion-button
+    >
+  </form>
+</template>
+
+<script>
+import {
+  IonList,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonTextarea,
+  IonButton,
+} from "@ionic/vue";
+export default {
+  components: {
+    IonList,
+    IonItem,
+    IonLabel,
+    IonInput,
+    IonTextarea,
+    IonButton,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/pages/AddMemoryPage.vue
+++ src/pages/AddMemoryPage.vue
@@ -1,44 +1,14 @@
 <template>
   <base-layout pageTitle="Nova memória" pageDefaultBackLink="/memories">
-    <form class="ion-padding">
-      <ion-list>
-        <ion-item>
-          <ion-label position="floating">Título</ion-label>
-          <ion-input type="text" required></ion-input>
-        </ion-item>
-        <ion-item>
-          <ion-label position="floating">Imagem</ion-label>
-          <ion-input type="url" required></ion-input>
-        </ion-item>
-      </ion-list>
-      <ion-item>
-        <ion-label position="floating">Descrição</ion-label>
-        <ion-textarea rows="5"></ion-textarea>
-      </ion-item>
-      <ion-button expand="full" class="ion-margin-top" color="tertiary"
-        >Salvar</ion-button
-      >
-    </form>
+    <create-memory-form></create-memory-form>
   </base-layout>
 </template>
 
 <script>
-import {
-  IonList,
-  IonItem,
-  IonLabel,
-  IonInput,
-  IonTextarea,
-  IonButton,
-} from "@ionic/vue";
+import CreateMemoryForm from "../components/memories/CreateMemoryForm";
 export default {
   components: {
-    IonList,
-    IonItem,
-    IonLabel,
-    IonInput,
-    IonTextarea,
-    IonButton,
+    CreateMemoryForm,
   },
 };
 </script>
